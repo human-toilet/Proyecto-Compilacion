bbef4f6010080a85ece7116863d51a11dce97d1d
Machi-san <maxibengochea67@gmail.com>
2024-03-29 18:06:33 -0400

    add invalid id

--- tokens.py
+++ tokens.py
@@ -1,6 +1,7 @@
 #palabras claves y operadores del lenguaje
 KEYWORDS: set = set()
 OPERATORS: set = set()
+INVALID_ID: set = set()
 
 with open('assets/keywords.txt', 'r') as f:
   for line in f.readlines():
@@ -9,6 +10,10 @@ with open('assets/keywords.txt', 'r') as f:
 with open('assets/operators.txt', 'r') as f:
   for line in f.readlines():
     OPERATORS.add(line.replace('\n', ''))
+    
+with open('assets/invalid_id.txt', 'r') as f:
+  for line in f.readlines():
+    INVALID_ID.add(line.replace('\n', ''))
     
 
   
