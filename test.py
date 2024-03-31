6cca61173554b7fe98cb2ea90695860a50053f68
P3ND4 <josecarlospendasrodriguez@gmail.com>
2024-03-31 16:56:05 -0400

    tester update

--- test.py
+++ test.py
@@ -4,7 +4,13 @@ from common.pycompiler import NonTerminal, Terminal
 from lexer.lexer import Lexer
 my_parser = LR1Parser(G, True)
 
-my_lexer = Lexer('1 + 2 = 3').tokens
+try: 
+    my_lexer = Lexer('1a + 2 = 3').tokens
+    parse = list(map(lambda x: sym_set[x.token_type], my_lexer))
+    print(my_parser(parse))
+except:
+    raise my_lexer
+    
 #print(sym_set[my_lexer[0].token_type])
-parse = list(map(lambda x: sym_set[x.token_type], my_lexer))
-print(my_parser(parse))
+
+
